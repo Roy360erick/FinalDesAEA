7a0c385153b1fdca045ff42f62e8bcc281152881
Erick Roy Fernandez Fernandez <efernandez@truenorth.pr>
2019-11-12 19:01:22 -0500

    add models

---
+++ Domain/Models/DetalleFactura.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class DetalleFactura
+    {
+        public int DetalleFacturaId { get; set; }
+
+        public int FacturaId { get; set; }
+        public virtual Factura Factura { get; set; }
+    }
+}
